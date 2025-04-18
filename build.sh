344acea0a341c4a5dec33443ab5bfb87bf7628c8
weapon <357958137@qq.com>
2025-04-18 10:25:45 +0800

    chore: 删除多余指令

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@
 # rustup target add x86_64-unknown-linux-musl
 # rustup target add aarch64-apple-darwin
 # rustup target add x86_64-apple-darwin
-# rustup target add arm64-apple-darwin
 
 # ~/.cargo/config.toml
 #   [target.x86_64-unknown-linux-musl]
@@ -14,4 +13,3 @@ cargo build --release --target x86_64-pc-windows-gnu
 cargo build --release --target x86_64-unknown-linux-musl
 cargo build --release --target aarch64-apple-darwin
 cargo build --release --target x86_64-apple-darwin
-cargo build --release --target arm64-apple-darwin
