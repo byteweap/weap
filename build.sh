70cf7091b3d609a97379355a67f62ed0c291b498
weapon <357958137@qq.com>
2025-04-18 10:23:06 +0800

    feat: 跨平台编译脚本

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# rustup target add x86_64-pc-windows-gnu
+# rustup target add x86_64-unknown-linux-musl
+# rustup target add aarch64-apple-darwin
+# rustup target add x86_64-apple-darwin
+# rustup target add arm64-apple-darwin
+
+# ~/.cargo/config.toml
+#   [target.x86_64-unknown-linux-musl]
+#   linker = "x86_64-linux-musl-gcc"
+
+cargo build --release --target x86_64-pc-windows-gnu
+cargo build --release --target x86_64-unknown-linux-musl
+cargo build --release --target aarch64-apple-darwin
+cargo build --release --target x86_64-apple-darwin
+cargo build --release --target arm64-apple-darwin
